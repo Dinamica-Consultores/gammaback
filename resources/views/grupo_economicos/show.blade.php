4f4be76200796acfa2f93ab76179123f788bef8b
Luis7592 <luisprador759@gmail.com>
2025-05-07 15:58:36 -0400

    Agregado

--- resources/views/grupo_economicos/show.blade.php
+++ resources/views/grupo_economicos/show.blade.php
@@ -6,12 +6,12 @@
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1>
-                    @lang('models/grupoEconomicos.singular') @lang('crud.detail')
+                    @lang('models/grupo_economicos.singular') @lang('crud.detail')
                     </h1>
                 </div>
                 <div class="col-sm-6">
                     <a class="btn btn-default float-right"
-                       href="{{ route('grupoEconomicos.index') }}">
+                       href="{{ route('grupo_economicos.index') }}">
                                                     @lang('crud.back')
                                             </a>
                 </div>
