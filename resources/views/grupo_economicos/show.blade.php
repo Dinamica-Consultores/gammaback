ae79c0e2cd0ad1b87f8fd5e59a65fed44a2805d3
Luis7592 <luisprador759@gmail.com>
2025-05-07 16:02:28 -0400

    Agregado

--- resources/views/grupo_economicos/show.blade.php
+++ resources/views/grupo_economicos/show.blade.php
@@ -6,7 +6,7 @@
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1>
-                    @lang('models/grupo_economicos.singular') @lang('crud.detail')
+             Red Comercial
                     </h1>
                 </div>
                 <div class="col-sm-6">
